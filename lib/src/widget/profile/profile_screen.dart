8ec83335cb7cd57d857bd9a315b51eeae23c2036
Thinh <v.thinhdv6@cloudhms.net>
2023-11-17 23:41:32 +0700

    [profile]:update profile in my profile

--- lib/src/widget/profile/profile_screen.dart
+++ lib/src/widget/profile/profile_screen.dart
@@ -2,8 +2,6 @@ import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
-import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:chewie/chewie.dart';
 import 'package:video_player/video_player.dart';
 import 'package:get/get.dart';
@@ -18,84 +16,127 @@ class Profile extends StatelessWidget {
     );
   }
 }
+
 class TikTokProfileScreen extends StatelessWidget {
   final VideoController videoController = Get.put(VideoController());
+  String? avatarUrl;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                CircleAvatar(
-                  radius: 40.0,
-                  backgroundImage: NetworkImage('https://example.com/your_profile_image.jpg'),
-                ),
-                SizedBox(height: 16.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Row(
-                      children: [
-                        Text('Đang follow'),
-                        Text('Follower'),
-                        Text('Thích'),
-                      ],
-                    ),
-                  ],
-                ),
-                SizedBox(height: 8.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text('Sửa hồ sơ'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text('Chia sẻ hồ sơ'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text('Thêm bạn'),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: GetBuilder<VideoController>(
-              builder: (controller) => ListView.builder(
-                itemCount: controller.videos.length,
-                itemBuilder: (context, index) {
-                  // Chỉ hiển thị video của người dùng đang đăng nhập
-                  if (controller.videos[index].user == FirebaseAuth.instance.currentUser?.uid) {
-                    return Container(
-                      height: 50.0,
-                      margin: EdgeInsets.symmetric(vertical: 8.0),
-                      child: Chewie(
-                        controller: ChewieController(
-                          videoPlayerController: VideoPlayerController.network(
-                            controller.videos[index].url,
-                          ),
-                          autoPlay: false,
-                          looping: false,
-                        ),
-                      ),
-                    );
-                  } else {
-                    return Container();
-                  }
-                },
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: NetworkImage(videoController.avatarUrl),
               ),
+              Positioned(
+                child: SizedBox(
+                  width: 30.0,
+                  height: 30.0,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      FilePickerResult? result =
+                          await FilePicker.platform.pickFiles(
+                        type: FileType.image,
+                      );
+                      if (result != null && result.files.isNotEmpty) {
+                        final selectedFile = result.files.single;
+                        if (selectedFile.path != null) {
+                          print('Selected Image Path: ${selectedFile.path}');
+                          File imageFile = File(selectedFile.path!);
+                          String userId =
+                              FirebaseAuth.instance.currentUser?.uid ?? '';
+                          await videoController.uploadAvatar(imageFile, userId);
+                        } else {
+                          print('Error: selectedFile.path is null.');
+                        }
+                      }
+                    },
+                    child: Icon(Icons.camera_alt),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 16.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                children: [
+                  Text('Đang follow'),
+                  Text('Follower'),
+                  Text('Thích'),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 8.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Sửa hồ sơ'),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Chia sẻ hồ sơ'),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Thêm bạn'),
+              ),
+            ],
+          ),
+
+          Expanded(
+            child: ListView.builder(
+              scrollDirection: Axis.vertical,
+              itemCount: (videoController.videos.length / 3).ceil(),
+              itemBuilder: (context, pageIndex) {
+                return SizedBox(
+                  height: 200.0,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: List.generate(
+                      3,
+                          (index) {
+                        final videoIndex = pageIndex * 3 + index;
+                        if (videoIndex < videoController.videos.length &&
+                            videoController.videos[videoIndex].user ==
+                                FirebaseAuth.instance.currentUser?.uid) {
+                          return Container(
+                            height: 150.0,
+                            width: MediaQuery.of(context).size.width / 3 - 3,
+                            margin: EdgeInsets.symmetric(horizontal: 0.7),
+                            child: Chewie(
+                              controller: ChewieController(
+                                videoPlayerController: VideoPlayerController.network(
+                                  videoController.videos[videoIndex].url,
+                                ),
+                                autoPlay: false,
+                                looping: false,
+                              ),
+                            ),
+                          );
+                        } else {
+                          return Container();
+                        }
+                      },
+                    ),
+                  ),
+                );
+              },
             ),
           ),
+
           ElevatedButton(
             onPressed: () async {
               FilePickerResult? result = await FilePicker.platform.pickFiles(
@@ -115,7 +156,6 @@ class TikTokProfileScreen extends StatelessWidget {
             },
             child: Text('Chọn và Upload Video'),
           ),
-
         ],
       ),
     );
