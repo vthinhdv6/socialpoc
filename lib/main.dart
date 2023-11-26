6bdc6bceedff5ada887cee3d3011d016358d36b5
Thaothuhute <nguyenthithuthao11102002@gmail.com>
2023-11-26 10:43:30 +0700

    thu thao : merge branch master

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:socialpoc/src/widget/login/login_email_screen.dart';
-
+import 'package:socialpoc/src/widget/login/register_email_screen.dart';
+import 'package:socialpoc/src/widget/main_screen.dart';
+import 'package:socialpoc/src/widget/profile/profile_screen.dart';
 
 import 'firebase_options.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 
-
 import 'src/widget/home.dart';
-
-
+import 'src/widget/list-notification/list_notification_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,6 +17,5 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await FirebaseAppCheck.instance.activate();
-  runApp(Register ());
+  runApp(MainScreen());
 }
-
